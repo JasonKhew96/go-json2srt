26b185cfce76cf5591b332e62bc4f4e76b595b6e
jasonkhew96 <29531167+JasonKhew96@users.noreply.github.com>
2021-10-05 20:50:21 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/JasonKhew96/go-json2srt
+
+go 1.17
+
+require github.com/asticode/go-astisub v0.18.0
+
+require (
+	github.com/asticode/go-astikit v0.21.1 // indirect
+	github.com/asticode/go-astits v1.10.0 // indirect
+	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
---
+++ main.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/asticode/go-astisub"
+)
+
+// json subtitle body
+type JsonSubBody struct {
+	From     float64 `json:"from"`     // start time (seconds)
+	To       float64 `json:"to"`       // end time (seconds)
+	Location int     `json:"location"` // subtitle location
+	Content  string  `json:"content"`  // subtitle content
+}
+
+// json subtitle
+type JsonSub struct {
+	FontSize        float64       `json:"font_size"`        // font size
+	FontColor       string        `json:"font_color"`       // font color in hex
+	BackgroundAlpha float64       `json:"background_alpha"` // font transparency
+	BackgroundColor string        `json:"background_color"` // font background color
+	Stroke          string        `json:"Stroke"`           // font stroke
+	Body            []JsonSubBody `json:"body"`             // json subtile body
+}
+
+// json2srt converts json subtitle into srt subtitle
+func json2srt(inputPath, outputPath string) error {
+	data, err := os.ReadFile(inputPath)
+	if err != nil {
+		return err
+	}
+
+	newSub := astisub.NewSubtitles()
+
+	index := 1
+
+	subtitle := &JsonSub{}
+	err = json.Unmarshal(data, subtitle)
+	if err != nil {
+		return err
+	}
+
+	var items []*astisub.Item
+
+	body := subtitle.Body
+	for _, value := range body {
+		from := value.From
+		content := value.Content
+		to := value.To
+
+		line := []astisub.Line{
+			{
+				Items: []astisub.LineItem{
+					{
+						Text: content,
+					},
+				},
+			},
+		}
+
+		item := &astisub.Item{
+			Index:   index,
+			EndAt:   time.Duration(to * float64(time.Second)),
+			Lines:   line,
+			StartAt: time.Duration(from * float64(time.Second)),
+		}
+		items = append(items, item)
+
+		index++
+	}
+
+	newSub.Items = items
+
+	f, err := os.OpenFile(outputPath, os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		return err
+	}
+
+	return newSub.WriteToSRT(f)
+}
+
+// srt2json converts srt subtitle into json subtitle
+func srt2json(inputPath, outputPath string) error {
+	sub, err := astisub.OpenFile(inputPath)
+	if err != nil {
+		return err
+	}
+	jsonSubtitle := &JsonSub{
+		FontSize:        0.4,
+		FontColor:       "#FFFFFF",
+		BackgroundAlpha: 0.5,
+		BackgroundColor: "#9C27B0",
+		Stroke:          "none",
+	}
+	for _, item := range sub.Items {
+		var lines []string
+		from := item.StartAt.Seconds()
+		to := item.EndAt.Seconds()
+		for _, line := range item.Lines {
+			for _, lineItem := range line.Items {
+				lines = append(lines, lineItem.Text)
+			}
+		}
+		newLine := strings.Join(lines, "\n")
+		jsonSubtitle.Body = append(jsonSubtitle.Body, JsonSubBody{
+			From:     from,
+			To:       to,
+			Location: 2,
+			Content:  newLine,
+		})
+	}
+
+	data, err := json.Marshal(jsonSubtitle)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(outputPath, data, 0666)
+}
+
+func main() {
+	arguments := os.Args[1:]
+	argsLen := len(arguments)
+	if argsLen != 2 {
+		fmt.Printf("Expected 2 arguments, but only found %d\nExample: convert input.json output.srt\n", argsLen)
+		return
+	}
+	inputPath := arguments[0]
+	outputPath := arguments[1]
+
+	var err error
+
+	inputExt := strings.ToLower(filepath.Ext(inputPath))
+	outputExt := strings.ToLower(filepath.Ext(outputPath))
+	switch inputExt {
+	case ".json":
+		if outputExt == ".srt" {
+			err = json2srt(inputPath, outputPath)
+		} else {
+			err = fmt.Errorf("\"%s\" file type is not supported", inputExt)
+		}
+	case ".srt":
+		if outputExt == ".json" {
+			err = srt2json(inputPath, outputPath)
+		} else {
+			err = fmt.Errorf("\"%s\" file type is not supported", inputExt)
+		}
+	default:
+		fmt.Printf("\"%s\" file type is not supported\n", inputExt)
+		return
+	}
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
